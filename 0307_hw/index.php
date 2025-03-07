5068c5ba82af83fe82af7bdaa1fafab8b882e74f
a1123355 <a1123355@mail.nuk.edu.tw>
2025-03-07 14:47:45 +0800

    info update

--- 0307_hw/index.php
+++ 0307_hw/index.php
@@ -10,8 +10,10 @@
     <form action = "./info.php" method = "GET">
     please input your name<input type ="text" name = "uName"><br/>
     please input your password<input type ="text" name = "uPwd"><br/>
-    <input type = "submit"><input type = "reset">
+    whatever<input type ="range"><br/>
+    <input type = "hidden" name = "uSecret" value = "123">
 
+    <input type = "submit"><input type = "reset">
     </form>
 </body>
 </html>
--- 0307_hw/info.php
+++ 0307_hw/info.php
@@ -1,6 +1,8 @@
 <?php
 $uName = $_GET["uName"];
 $uPwd = $_GET["uPwd"];
+$uSecret = $_GET["uSecret"];
 echo "your name is ".$uName."<br>";
-echo "your password is ".$uPwd;
+echo "your password is ".$uPwd."<br>";
+echo "your Secret is ".$uSecret;
 ?>
