016739305e745e5cde2792ab9b7c0ca7b9e73ddd
a1123355 <a1123355@mail.nuk.edu.tw>
2025-03-07 14:16:10 +0800

    add 0307 homework

---
+++ 0307_hw/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "./info.php" method = "GET">
+    please input your name<input type ="text" name = "uName"><br/>
+    please input your password<input type ="text" name = "uPwd"><br/>
+    <input type = "submit"><input type = "reset">
+
+    </form>
+</body>
+</html>
---
+++ 0307_hw/info.php
@@ -0,0 +1,6 @@
+<?php
+$uName = $_GET["uName"];
+$uPwd = $_GET["uPwd"];
+echo "your name is ".$uName."<br>";
+echo "your password is ".$uPwd;
+?>
