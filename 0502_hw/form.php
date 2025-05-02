1df38db72c58e1519ada3b52472231ad2c556b68
a1123355 <a1123355@mail.nuk.edu.tw>
2025-05-02 15:37:11 +0800

    update

---
+++ 0502_hw/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "show.app" method = "post">
+        username = <input type = "text" name = "uname"><br>
+        password = <input type = "password" name = "uname"><br>
+    <input type = "submit">
+    </form>
+</body>
+</html>
